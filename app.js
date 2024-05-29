15e2b179886cdbf057da4bacde64b3d4d947974d
Aditya-1322 <kaushikaditya985@gmail.com>
2024-05-29 11:29:22 +0530

    client connect

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const express = require("express")
-const socket = require('socket.io')
-const http = require('http')
-const {chess } = require('chess.js')
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
+const { title } = require("process");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let player = {};
+let currentPlayer = "W";
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Chess Game" });
+});
+
+io.on("connection", (uniqueSocket) => {
+  // uniqueSocket is a unique value of user
+  console.log("connected");
+
+  uniqueSocket.on("Ram", () => {
+    console.log("Jai shree Ram");
+  });
+});
+server.listen(3000, () => {
+  console.log(`Server is running on Port:3000`);
+});
