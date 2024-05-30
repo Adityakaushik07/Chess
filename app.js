0f1f7ab00f6882c45258c323b370b767e3f49a2c
Aditya-1322 <kaushikaditya985@gmail.com>
2024-05-30 11:29:20 +0530

    backend setup complete

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { Chess } = require("chess.js");
 const path = require("path");
 const { title } = require("process");
 
+
+
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
@@ -23,9 +25,46 @@ io.on("connection", (uniqueSocket) => {
   // uniqueSocket is a unique value of user
   console.log("connected");
 
-  uniqueSocket.on("Ram", () => {
-    console.log("Jai shree Ram");
+  if (!player.white) {
+    player.white = uniqueSocket.id;
+    uniqueSocket.emit("plyerRole", "w");
+  } else if (!player.black) {
+    player.black = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "b");
+  } else {
+    uniqueSocket.emit("spectatorRole");
+  }
+
+  uniqueSocket.on("disconnect", () => {
+    if (uniqueSocket.id === player.white) {
+      delete player.white;
+    } else if (uniqueSocket.id === player.black) {
+      delete player.black;
+    }
   });
+
+  uniqueSocket.on("move",(move)=>{
+    try {
+      if(chess.turn()=== "w" && uniqueSocket.id !== player.white) return
+      if(chess.turn()=== "b" && uniqueSocket.id !== player.black) return
+
+      const result = chess.move(move)
+      if (result) {
+        currentPlayer = chess.turn()
+        io.emit("move",move)
+        io.emit("boardState", chess.fen())
+      }
+      else{
+        console.log("Invalid Move : ", move)
+        uniqueSocket.emit("invalidMove", move)
+      }
+      } catch (err) {
+        console.log(err)
+        uniqueSocket.emit("Invalid Move : ", move)
+      
+    }
+  })
+
 });
 server.listen(3000, () => {
   console.log(`Server is running on Port:3000`);
